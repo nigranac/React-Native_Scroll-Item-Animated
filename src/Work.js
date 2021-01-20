cabd6cbf669b466a409f9cc4ed9a63f3eb2a73d5
nigranac <can.argin.16@gmail.com>
2021-01-21 00:01:56 +0300

    addImageAndStyle

--- src/Work.js
+++ src/Work.js
@@ -32,31 +32,60 @@ const DATA = [...Array(30).keys()].map((_, i) => {
 
 const SPACING = 20;
 const AVATAR_SIZE = 70;
+const BG="https://www.pexels.com/photo/pink-rose-closeup-photography-1231265/"
 
 const Work = () => {
   return (
     <View style={{flex: 1, backgroundColor: '#fff'}}>
       <StatusBar hidden />
+     <Image
+     source={{uri:BG}}
+     style={StyleSheet.absoluteFillObject}
+     blurRadius={80}
+     
+     />
 
       <FlatList
         data={DATA}
         keyExtractor={(item) => item.key}
+        contentContainerStyle={{
+          padding: SPACING,
+          paddingTop: StatusBar.currentHeight || 42,
+        }}
         renderItem={({item, index}) => {
           return (
-            <View style={{flexDirection:"row",padding:SPACING,marginBottom:SPACING}}>
+            <View
+              style={{
+                flexDirection: 'row',
+                padding: SPACING,
+                marginBottom: SPACING,
+                shadowColor: '#000',
+                shadowOffset: {
+                  width: 0,
+                  height: 10,
+                },
+                shadowOpacity: 0.3,
+                shadowRadius: 20,
+              }}>
               <Image
                 source={{uri: item.image}}
                 style={{
                   width: AVATAR_SIZE,
                   height: AVATAR_SIZE,
                   borderRadius: AVATAR_SIZE,
-                  marginRight:SPACING/2
+                  marginRight: SPACING / 2,
                 }}
               />
               <View>
-                  <Text style={{fontSize:22,fontWeight:"700"}}>{item.name}</Text>
-                  <Text style={{fontSize:18,opacity:.7}}>{item.jobTitle}</Text>
-                  <Text style={{fontSize:14,opacity:.8,color:"#0099cc"}}>{item.email}</Text>
+                <Text style={{fontSize: 22, fontWeight: '700'}}>
+                  {item.name}
+                </Text>
+                <Text style={{fontSize: 18, opacity: 0.7}}>
+                  {item.jobTitle}
+                </Text>
+                <Text style={{fontSize: 14, opacity: 0.8, color: '#0099cc'}}>
+                  {item.email}
+                </Text>
               </View>
             </View>
           );
